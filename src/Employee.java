6dc95086b40a9a9981ca519546f77d6473d3a467
alirt <alirtaskiran@gmail.com>
2022-09-26 11:14:37 +0300

    .

---
+++ src/Employee.java
@@ -0,0 +1,3 @@
+public class Employee extends Person {             //Employee = calisan, salary = maas
+    double salary;
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        Customer customer = new Customer();
+        Employee employee = new Employee();
+        EmployeeManager employeeManager = new EmployeeManager();
+        CustomerManager customerManager = new CustomerManager();
+
+        /*customer.email = "alirtaskiran@gmail.com";
+        employee.salary = 10000;
+        customerManager.add();
+        employeeManager.bestEmployee();*/
+
+    }
+}
